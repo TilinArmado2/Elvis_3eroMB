4a2270fd7396438398613929acaa41967b0c5a98
augustocrafter22-lab <augustocrafter22@gmail.com>
2026-06-12 16:33:08 -0300

    DOM

---
+++ assets/js/empleados.js
@@ -0,0 +1,61 @@
+const formEmpleado = document.getElementById("formEmpleado");
+
+formEmpleado.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const cedula = document.getElementById("ciEmpleado").value;
+    const nombre = document.getElementById("nombreEmpleado").value;
+    const apellido = document.getElementById("apellidoEmpleado").value;
+    const cargo = document.getElementById("cargoEmpleado").value;
+
+    const empleado = {
+        cedula: cedula,
+        nombre: nombre,
+        apellido: apellido,
+        cargo: cargo
+    };
+
+    const fila = document.createElement("tr");
+
+    const celdaCedula = document.createElement("td");
+    celdaCedula.textContent = empleado.cedula;
+
+    const celdaNombre = document.createElement("td");
+    celdaNombre.textContent = empleado.nombre;
+
+    const celdaApellido = document.createElement("td");
+    celdaApellido.textContent = empleado.apellido;
+
+    const celdaCargo = document.createElement("td");
+    celdaCargo.textContent = empleado.cargo;
+
+    const celdaAcciones = document.createElement("td");
+
+    const btnModificar = document.createElement("button");
+    btnModificar.textContent = "Modificar";
+    btnModificar.addEventListener("click", function() {
+const celdas = fila.children;
+});
+});
+
+    const btnEliminar = document.createElement("button");
+    btnEliminar.textContent = "Eliminar";
+
+    btnEliminar.addEventListener("click", function() {
+        fila.remove();
+    });
+
+    celdaAcciones.appendChild(btnModificar);
+    celdaAcciones.appendChild(btnEliminar);
+
+    fila.appendChild(celdaCedula);
+    fila.appendChild(celdaNombre);
+    fila.appendChild(celdaApellido);
+    fila.appendChild(celdaCargo);
+    fila.appendChild(celdaAcciones);
+
+    const cuerpoTabla = document.getElementById("cuerpoTablaEmpleado");
+    cuerpoTabla.appendChild(fila);
+
+    formEmpleado.reset();
+});
